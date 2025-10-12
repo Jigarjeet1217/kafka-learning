c29023061499cb5c827e4bf5f1cf04498c5c9562
JigarjeetSingh <jigarjeet1217@gmail.com>
2025-10-12 20:08:42 +0530

    kafka setup properties and error handling

--- pushDataEvents/kafka.js
+++ pushDataEvents/kafka.js
@@ -1,12 +1,16 @@
 let { Kafka, logLevel, Partitioners } = require('kafkajs');
 const { resolve } = require('path');
 
+// handling kafka config / setup properties
 let kafka = new Kafka({
   clientId: process.env.clientId,
   brokers: ['localhost:9092'],
+  retry: {
+    maxRetryTime: 20000, // how long to continue retry mechanism
+    initialRetryTime: 100, // start retry after what time in ms
+    retries: 7, // max number of retries - max value = 8
+  }
 });
-//
-// Partitioners
 
 let producer = kafka.producer();
 
@@ -15,7 +19,10 @@ async function main() {
     await producer.connect();
     resolve('success');
   } catch (error) {
-    console.log('error in producer connection', error);
+    console.log('error in producer connection', {
+      name: error.name,
+      message: error.message,
+    });
   }
 }
 
